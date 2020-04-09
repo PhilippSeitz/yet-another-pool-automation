861fe06205b540f314bd0423690c2f7ee5a31997
Philipp Seitz <44158030+PhilippSeitz@users.noreply.github.com>
2020-04-10 00:06:09 +0200

    toggle-card clickable

--- libs/view/shared/src/lib/components/toggle-card/toggle-card.component.ts
+++ libs/view/shared/src/lib/components/toggle-card/toggle-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'pool-toggle-card',
@@ -6,7 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./toggle-card.component.scss']
 })
 export class ToggleCardComponent implements OnInit {
+  @Input() checked = false;
+  @Output() checkedChange = new EventEmitter<boolean>();
   constructor() {}
 
   ngOnInit(): void {}
+
+  click(event: MouseEvent) {
+    this.checked = !this.checked;
+    event.preventDefault();
+  }
 }
--- libs/view/shared/src/lib/components/toggle/toggle.component.ts
+++ libs/view/shared/src/lib/components/toggle/toggle.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'pool-toggle',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./toggle.component.scss']
 })
 export class ToggleComponent implements OnInit {
+  @Input() checked: boolean;
+  
   constructor() {}
 
   ngOnInit(): void {}
