18e3f16a761fef3c86e5ae8ca4eddcdfd04b15b6
Philipp Seitz <44158030+PhilippSeitz@users.noreply.github.com>
2020-04-13 16:48:14 +0200

    🧼 purgecss

---
+++ webpack-prod.config.js
@@ -0,0 +1,26 @@
+const purgecss = require('@fullhuman/postcss-purgecss');
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        loader: 'postcss-loader',
+        options: {
+          ident: 'postcss',
+          syntax: 'postcss-scss',
+          plugins: () => [
+            require('postcss-import'),
+            require('tailwindcss'),
+            require('autoprefixer'),
+            purgecss({
+              content: ['./**/*.html'],
+              defaultExtractor: content =>
+                content.match(/[\w-/:]+(?<!:)/g) || []
+            })
+          ]
+        }
+      }
+    ]
+  }
+};
