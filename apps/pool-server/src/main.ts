1a193cd29beca5059eee1f4efd3b2e4376994103
Philipp Seitz <44158030+PhilippSeitz@users.noreply.github.com>
2020-04-13 22:49:33 +0200

    generate small package.json for node apps

--- apps/pool-server/src/main.ts
+++ apps/pool-server/src/main.ts
@@ -1,12 +1,6 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
-import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
